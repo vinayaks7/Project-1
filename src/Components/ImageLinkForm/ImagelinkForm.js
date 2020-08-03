959c06b21998063a3ec84c59d64313c3cddf3134
vinayaks7 <63201376+vinayaks7@users.noreply.github.com>
2020-08-03 14:20:10 +0530

    Add files via upload

---
+++ src/Components/ImageLinkForm/ImagelinkForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './ImagelinkForm.css';
+
+const ImageLinkForm = ({onInputChange,onButtonSubmit}) => {
+
+    return (
+
+        <div>
+            <p className='f3'>
+                {
+                    'This Magic Brain will detect faces in your picture. Lets Check'
+                }
+
+            </p>
+            <div className='center'>
+                
+                <div className='form center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-90 center ' type='tex' onChange={onInputChange}/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+                    
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+    );
+}
+
+export default ImageLinkForm;
---
+++ src/Rank/Rank.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Rank = () => {
+
+    return (
+
+        <div>
+            <div className='red f3'>
+                {
+                    'Vinayak ,your current rank is ....'
+                }
+            </div>
+            <div className='red f1'>
+                {'#5'}
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+    );
+}
+
+export default Rank;
